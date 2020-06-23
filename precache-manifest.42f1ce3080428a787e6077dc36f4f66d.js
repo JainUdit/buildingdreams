99250bba9d7a104e06e28f435365f3b1010a18fe
Udit Jain <jain_udit@infogain.com>
2020-06-23 19:37:25 +0530

    Updates

---
+++ precache-manifest.42f1ce3080428a787e6077dc36f4f66d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac6f8b4b5be011716ee5a170285bb777",
+    "url": "/JainUdit/buildingdreams.git/index.html"
+  },
+  {
+    "revision": "ab277feea25a6829dba7",
+    "url": "/JainUdit/buildingdreams.git/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a8bd7c71d866b8c6b0e4",
+    "url": "/JainUdit/buildingdreams.git/static/js/2.da89e792.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/JainUdit/buildingdreams.git/static/js/2.da89e792.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab277feea25a6829dba7",
+    "url": "/JainUdit/buildingdreams.git/static/js/main.9b325c53.chunk.js"
+  },
+  {
+    "revision": "0891732d337f4fec96d2",
+    "url": "/JainUdit/buildingdreams.git/static/js/runtime-main.a9ae4190.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/JainUdit/buildingdreams.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
